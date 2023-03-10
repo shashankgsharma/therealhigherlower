f29de9a6f9832ec20bb7b54ca954d282b8e94bf9
SHASHANK G SHARMA <77768375+shashankgsharma@users.noreply.github.com>
2023-03-10 03:23:56 +0000

    Initial commit

---
+++ game_data.py
@@ -0,0 +1,117 @@
+male_data = [
+  #Zimbawe
+  {'name': 'Andy Flower', 'international_runs': 11314, 'age': 53, 'country': 'Zimbabwe', 'international_wickets': 75},
+  {'name': 'Grant Flower', 'international_runs': 11104, 'age': 51, 'country': 'Zimbabwe', 'international_wickets': 81},
+  {'name': 'Brendan Taylor', 'international_runs': 11552, 'age': 36, 'country': 'Zimbabwe', 'international_wickets': 102},
+  {'name': 'Sean Williams', 'international_runs': 5633, 'age': 35, 'country': 'Zimbabwe', 'international_wickets': 59},
+  {'name': 'Heath Streak', 'international_runs': 4892, 'age': 47, 'country': 'Zimbabwe', 'international_wickets': 288}, 
+  #South Africa
+  {'name': 'Jacques Kallis', 'international_runs': 25534, 'age': 46, 'country': 'South Africa', 'international_wickets': 577},
+  {'name': 'Graeme Smith', 'international_runs': 17082, 'age': 41, 'country': 'South Africa', 'international_wickets': 8},
+  {'name': 'AB de Villiers', 'international_runs': 19754, 'age': 37, 'country': 'South Africa', 'international_wickets': 97}, 
+  {'name': 'Hashim Amla', 'international_runs': 18616, 'age': 38, 'country': 'South Africa', 'international_wickets': 76},
+  {'name': 'Gary Kirsten', 'international_runs': 18261, 'age': 54, 'country': 'South Africa', 'international_wickets': 56}, 
+  #Sri Lanka
+  {'name': 'Kumar Sangakkara', 'international_runs': 28016, 'age': 44, 'country': 'Sri Lanka', 'international_wickets': 0}, 
+  {'name': 'Mahela Jayawardene', 'international_runs': 25957, 'age': 44, 'country': 'Sri Lanka', 'international_wickets': 0},
+  {'name': 'Sanath Jayasuriya', 'international_runs': 21032, 'age': 52, 'country': 'Sri Lanka', 'international_wickets': 323},
+  {'name': 'Tillakaratne Dilshan', 'international_runs': 17486, 'age': 45, 'country': 'Sri Lanka', 'international_wickets': 66}, 
+  {'name': 'Aravinda de Silva', 'international_runs': 16702, 'age': 56, 'country': 'Sri Lanka', 'international_wickets': 136}, 
+   #Bangladesh
+  {'name': 'Mushfiqur Rahim', 'international_runs': 8295, 'age': 34, 'country': 'Bangladesh', 'international_wickets': 2},
+  {'name': 'Tamim Iqbal', 'international_runs': 14305, 'age': 32, 'country': 'Bangladesh', 'international_wickets': 1},
+  {'name': 'Shakib Al Hasan', 'international_runs': 12147, 'age': 34, 'country': 'Bangladesh', 'international_wickets': 532},
+  {'name': 'Mahmudullah', 'international_runs': 7032, 'age': 36, 'country': 'Bangladesh', 'international_wickets': 83},
+  {'name': 'Mashrafe Mortaza', 'international_runs': 2825, 'age': 38, 'country': 'Bangladesh', 'international_wickets': 269}, 
+  #West Indies
+  {'name': 'Brian Lara', 'international_runs': 22358, 'age': 52, 'country': 'West Indies', 'international_wickets': 31},
+  {'name': 'Chris Gayle', 'international_runs': 19020, 'age': 42, 'country': 'West Indies', 'international_wickets': 101},
+  {'name': 'Shivnarine Chanderpaul', 'international_runs': 20957, 'age': 47, 'country': 'West Indies', 'international_wickets': 47},
+  {'name': 'Vivian Richards', 'international_runs': 15328, 'age': 69, 'country': 'West Indies', 'international_wickets': 118},
+  {'name': 'Desmond Haynes', 'international_runs': 18433, 'age': 65, 'country': 'West Indies', 'international_wickets': 13},
+  #Pakistan
+  {'name': 'Inzamam-ul-Haq', 'international_runs': 20069, 'age': 51, 'country': 'Pakistan', 'international_wickets': 48},
+  {'name': 'Javed Miandad', 'international_runs': 16345, 'age': 64, 'country': 'Pakistan', 'international_wickets': 16},
+  {'name': 'Mohammad Yousuf', 'international_runs': 16100, 'age': 47, 'country': 'Pakistan', 'international_wickets': 39},
+  {'name': 'Shoaib Malik', 'international_runs': 11148, 'age': 40, 'country': 'Pakistan', 'international_wickets': 233},
+  {'name': 'Saleem Malik', 'international_runs': 11667, 'age': 58, 'country': 'Pakistan', 'international_wickets': 161},
+  #New Zealand
+  {'name': 'Kane Williamson', 'international_runs': 10803, 'international_wickets': 29, 'age': 31, 'country': 'New Zealand'},
+  {'name': 'Ross Taylor', 'international_runs': 17940, 'international_wickets': 1, 'age': 37, 'country': 'New Zealand'},
+  {'name': 'Brendon McCullum', 'international_runs': 14071, 'international_wickets': 39, 'age': 40, 'country': 'New Zealand'},
+  {'name': 'Stephen Fleming', 'international_runs': 15608, 'international_wickets': 32, 'age': 48, 'country': 'New Zealand'},
+  {'name': 'Martin Guptill', 'international_runs': 9374, 'international_wickets': 2, 'age': 35, 'country': 'New Zealand'},
+  #England
+  {'name': 'Joe Root', 'international_runs': 14753, 'international_wickets': 26, 'age': 31, 'country': 'England'},
+  {'name': 'Eoin Morgan', 'international_runs': 10476, 'international_wickets': 0, 'age': 35, 'country': 'England'},
+  {'name': 'Kevin Pietersen', 'international_runs': 13398, 'international_wickets': 7, 'age': 41, 'country': 'England'},
+  {'name': 'Alastair Cook', 'international_runs': 21805, 'international_wickets': 1, 'age': 37, 'country': 'England'},
+  {'name': 'Andrew Flintoff', 'international_runs': 7984, 'international_wickets': 399, 'age': 43, 'country': 'England'},
+  #Australia
+  {'name': 'Steve Smith', 'international_runs': 18825, 'international_wickets': 17, 'age': 32, 'country': 'Australia'},
+  {'name': 'David Warner', 'international_runs': 15126, 'international_wickets': 1, 'age': 35, 'country': 'Australia'},
+  {'name': 'Ricky Ponting', 'international_runs': 27483, 'international_wickets': 5, 'age': 47, 'country': 'Australia'},
+  {'name': 'Glenn McGrath', 'international_runs': 641, 'international_wickets': 949, 'age': 51, 'country': 'Australia'},
+  {'name': 'Adam Gilchrist', 'international_runs': 16203, 'international_wickets': 0, 'age': 50, 'country': 'Australia'},
+  #India
+  {'name': 'Virat Kohli', 'international_runs': 22559, 'age': 33, 'country': 'India', 'international_wickets': 4},
+  {'name': 'Rohit Sharma', 'international_runs': 14320, 'age': 34, 'country': 'India', 'international_wickets': 8},
+  {'name': 'Shikhar Dhawan', 'international_runs': 12387, 'age': 36, 'country': 'India', 'international_wickets': 2},
+  {'name': 'Sachin Tendulkar', 'international_runs': 34357, 'age': 48, 'country': 'India', 'international_wickets': 46}, 
+  {'name': 'Yuvraj Singh', 'international_runs': 10601, 'age': 40, 'country': 'India', 'international_wickets': 148}
+
+]
+
+
+female_data = [
+  
+  #Bangladesh
+  {'name': 'Salma Khatun', 'international_runs': 1381, 'international_wickets': 73, 'age': 31, 'country': 'Bangladesh'},
+  {'name': 'Rumana Ahmed', 'international_runs': 1822, 'international_wickets': 36, 'age': 30, 'country': 'Bangladesh'},
+  {'name': 'Fargana Hoque', 'international_runs': 1205, 'international_wickets': 6, 'age': 25, 'country': 'Bangladesh'},
+  {'name': 'Nigar Sultana', 'international_runs': 344, 'international_wickets': 0, 'age': 26, 'country': 'Bangladesh'},
+  {'name': 'Jahanara Alam', 'international_runs': 701, 'international_wickets': 84, 'age': 28, 'country': 'Bangladesh'}, 
+  #Thailand
+  {'name': 'Nattaya Boochatham', 'international_runs': 123, 'international_wickets': 10, 'age': 30, 'country': 'Thailand'},
+  {'name': 'Naruemol Chaiwai', 'international_runs': 516, 'international_wickets': 0, 'age': 27, 'country': 'Thailand'},
+  {'name': 'Sornnarin Tippoch', 'international_runs': 1096, 'international_wickets': 90, 'age': 34, 'country': 'Thailand'},
+  {'name': 'Onnicha Kamchomphu', 'international_runs': 98, 'international_wickets': 5, 'age': 28, 'country': 'Thailand'},
+  {'name': 'Ratanaporn Padunglerd', 'international_runs': 50, 'international_wickets': 2, 'age': 25, 'country': 'Thailand'}, 
+  #New Zealand
+  {'name': 'Suzie Bates', 'international_runs': 8064, 'international_wickets': 110, 'age': 34, 'country': 'New Zealand'}, 
+  {'name': 'Sophie Devine', 'international_runs': 5317, 'international_wickets': 85, 'age': 32, 'country': 'New Zealand'},
+  {'name': 'Amy Satterthwaite', 'international_runs': 3783, 'international_wickets': 52, 'age': 34, 'country': 'New Zealand'},
+  {'name': 'Katey Martin', 'international_runs': 1766, 'international_wickets': 0, 'age': 36, 'country': 'New Zealand'},
+  {'name': 'Lea Tahuhu', 'international_runs': 357, 'international_wickets': 69, 'age': 31, 'country': 'New Zealand'}, 
+  #West Indies
+  {'name': 'Stafanie Taylor', 'international_runs': 4544, 'age': 30, 'country': 'West Indies', 'international_wickets': 102},
+  {'name': 'Deandra Dottin', 'international_runs': 2724, 'age': 29, 'country': 'West Indies', 'international_wickets': 60},
+  {'name': 'Hayley Matthews', 'international_runs': 1433, 'age': 23, 'country': 'West Indies', 'international_wickets': 33},
+  {'name': 'Merissa Aguilleira', 'international_runs': 1697, 'age': 36, 'country': 'West Indies', 'international_wickets': 64},
+  {'name': 'Anisa Mohammed', 'international_runs': 888, 'age': 33, 'country': 'West Indies', 'international_wickets': 151}, 
+  #South Africa
+  {'name': 'Mignon du Preez', 'international_runs': 3818, 'age': 32, 'country': 'South Africa', 'international_wickets': 4},
+  {'name': 'Dane van Niekerk', 'international_runs': 2341, 'age': 28, 'country': 'South Africa', 'international_wickets': 125},
+  {'name': 'Chloe Tryon', 'international_runs': 1422, 'age': 27, 'country': 'South Africa', 'international_wickets': 47},
+  {'name': 'Marizanne Kapp', 'international_runs': 2225, 'age': 31, 'country': 'South Africa', 'international_wickets': 129},
+  {'name': 'Shabnim Ismail', 'international_runs': 395, 'age': 32, 'country': 'South Africa', 'international_wickets': 149}, 
+  #England
+  {'name': 'Charlotte Edwards', 'international_runs': 5992, 'age': 42, 'country': 'England', 'international_wickets': 23},
+  {'name': 'Clare Taylor', 'international_runs': 2039, 'age': 45, 'country': 'England', 'international_wickets': 105},
+  {'name': 'Lydia Greenway', 'international_runs': 2033, 'age': 36, 'country': 'England', 'international_wickets': 2},
+  {'name': 'Sarah Taylor', 'international_runs': 4082, 'age': 32, 'country': 'England', 'international_wickets': 0},
+  {'name': 'Heather Knight', 'international_runs': 3164, 'age': 30, 'country': 'England', 'international_wickets': 35}, 
+  #Australia
+  {'name': 'Belinda Clark', 'international_runs': 4854, 'age': 51, 'country': 'Australia', 'international_wickets': 45},
+  {'name': 'Karen Rolton', 'international_runs': 4074, 'age': 46, 'country': 'Australia', 'international_wickets': 106},
+  {'name': 'Meg Lanning', 'international_runs': 5616, 'age': 29, 'country': 'Australia', 'international_wickets': 8},
+  {'name': 'Ellyse Perry', 'international_runs': 4418, 'age': 31, 'country': 'Australia', 'international_wickets': 305},
+  {'name': 'Alex Blackwell', 'international_runs': 3944, 'age': 38, 'country': 'Australia', 'international_wickets': 5},
+  #India
+  {'name': 'Mithali Raj', 'international_runs': 10127, 'age': 39, 'country': 'India', 'international_wickets': 8},
+  {'name': 'Jhulan Goswami', 'international_runs': 2318, 'age': 38, 'country': 'India', 'international_wickets': 347},
+  {'name': 'Harmanpreet Kaur', 'international_runs': 2655, 'age': 32, 'country': 'India', 'international_wickets': 26},
+  {'name': 'Smriti Mandhana', 'international_runs': 3477, 'age': 25, 'country': 'India', 'international_wickets': 4},
+  {'name': 'Poonam Yadav', 'international_runs': 174, 'age': 30, 'country': 'India', 'international_wickets': 150}
+
+]
---
+++ main.py
@@ -0,0 +1,75 @@
+from os import system
+from time import sleep
+from random import choice
+from game_data import male_data, female_data
+from art import logo, vs
+
+def random_player_selector(data):
+    return choice(data)
+
+
+def print_player(player, gender):
+    return f"{player['name']}, a {player['age']} years old {gender}s cricketer from {player['country']}, having taken {player['international_wickets']} international wickets.\n"
+
+
+def compare(a, b):
+    if a > b:
+        return "l"
+    elif a < b:
+        return "h"
+    else:
+        return "pass"
+
+
+def play():
+    print(logo)
+    ans1 = input(
+        "Type 'm' to play Men's Cricket Trivia, 'w' to play Women's Cricket Trivia: "
+    ).lower()
+
+    if ans1 == 'w':
+        data = female_data
+        gender = 'Women'
+    else:
+        data = male_data
+        gender = 'Men'
+
+    player1 = random_player_selector(data)
+    player2 = random_player_selector(data)
+
+    game_end = False
+    score = 0
+
+    while not game_end:
+        system('clear')
+        print(logo + "\n")
+        print(f"Score: {score}.\n")
+        print(print_player(player1, gender))
+        print(vs)
+        print(print_player(player2, gender))
+
+        result = compare(player1['international_runs'],
+                         player2['international_runs'])
+        user_guess = input(
+            f"Type 'h' if you think {player2['name']}'s international runs is higher than {player1['name']}'s international runs, \nType 'l' if you think it's lower: \n"
+        ).lower()
+
+        print(f"\nInternational runs:\n{player1['name']}: {player1['international_runs']} runs.\n{player2['name']}: {player2['international_runs']} runs.\n")
+        sleep(4)
+        if user_guess == result:
+            score += 1
+            player1 = player2
+            player2 = random_player_selector(data)
+        else:
+            game_end = True
+    print(f"\nGame ended, final_score: {score}.\n")
+
+
+while (input(
+        "Wanna play a game of higher-lower? (Type 'y' to play, 'n' to end): ").
+       lower() == 'y'):
+    system('clear')
+    play()
+
+system('clear')
+print('GoodBye!')
